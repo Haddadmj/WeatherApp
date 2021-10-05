31bf0bdaafc6a4db3c6d4887c5aaaed9cb9e85cf
Mohammad Al-Haddad <moh.jalhaddad@gmail.com>
2021-10-05 21:47:45 +0300

    Added API and Model

---
+++ app/src/main/java/com/example/weatherapp/api/APIInterface.kt
@@ -0,0 +1,16 @@
+package com.example.weatherapp.api
+
+import com.example.weatherapp.model.WeatherResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface APIInterface {
+    @GET("/weather")
+    fun getWeatherByZIP(
+        @Query("zip") zip:String,
+        @Query("appid") appid:String = "e840c236f28359e47f483dfbe723beb5",
+        @Query("units") units: String ="metric",
+    ) : Call<WeatherResponse>?
+
+}
