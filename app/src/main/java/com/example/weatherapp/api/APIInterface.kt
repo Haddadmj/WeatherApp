48391c0f66c9f122eee8e540fa2d83807fac4d55
Mohammad Al-Haddad <moh.jalhaddad@gmail.com>
2021-10-06 15:19:36 +0300

    Finished App Except Corutines

--- app/src/main/java/com/example/weatherapp/api/APIInterface.kt
+++ app/src/main/java/com/example/weatherapp/api/APIInterface.kt
@@ -6,11 +6,11 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface APIInterface {
-    @GET("/weather")
+    @GET("weather")
     fun getWeatherByZIP(
-        @Query("zip") zip:String,
+        @Query("zip", encoded = true) zip:String,
         @Query("appid") appid:String = "e840c236f28359e47f483dfbe723beb5",
         @Query("units") units: String ="metric",
-    ) : Call<WeatherResponse>?
+    ) : Call<WeatherResponse?>
 
 }
